a616898436412a4fff62e1a14419521370ec6469
Mehdi <mehdi.wachani@edu.ece.fr>
2020-04-14 00:01:52 +0200

    Page de co vendeur et formulaire 1er co

---
+++ vendre.php
@@ -0,0 +1,36 @@
+<?php
+//recuperer les données venant de la page HTML Connexion
+$login = isset($_POST["LoginC"])? $_POST["LoginC"] : "";
+$mdp = isset($_POST["mdpC"])? $_POST["mdpC"] : "";
+
+
+$serveur = array(
+    "dodo" => "12345",
+    "mehdi" => "6789",
+);
+
+$connexion = false;
+for ($i = 0; $i < count($serveur); $i++) {
+    if ($serveur[$login] == $mdp){
+    $connexion = true;
+    break;}
+}
+
+
+if ($connexion) {
+    if (isset($_POST['button'])) {
+        echo "Login successfull !";
+        header("Location: home.html");
+    }
+    if (isset($_POST['button2']))
+    {
+        echo "Login NOUVEAU successfull !";
+        header("Location: vendeur.html");
+    }
+}
+else
+{
+    echo "Vous n'etes pas dans le serveur";
+}
+
+?>
